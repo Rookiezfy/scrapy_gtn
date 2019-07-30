02cff8d518091bf2be89b8662d83e8a9212c0f74
jt_zfy <1037957472@qq.com>
2019-07-30 15:05:11 +0800

    从执行命令获取开始日期参数，一次性获取开始日期到当前日期的所有日、周、月及分钟各个频度的数据

--- scrapy_gtn/conf/config.py
+++ scrapy_gtn/conf/config.py
@@ -2,9 +2,8 @@ class GlobalVar:
     # ts token
     ts_token = '87ca2f52711feb88ff84df57ad8f350662499b563bd9f48e917bc5e2'
 
-    root_dir = 'E:/01git/python/scrapy_gtn/scrapy_gtn/'
-    # 结果文件存放路径
-    data_dir = root_dir + 'data/'
+    # 结果文件存放路径 相对于根路径E:/01git/python/scrapy_gtn/
+    data_dir = '/scrapy_gtn/data/'
 
     # 日志文件格式
     log_format = '[%(levelname)s]%(asctime)s %(name)s: %(message)s'
@@ -13,7 +12,7 @@ class GlobalVar:
     log_datefmt = '%Y-%m-%d %H:%M:%S'
 
     # 日志文件存放路径
-    log_dir = root_dir + 'log/'
+    log_dir = '/scrapy_gtn/log/'
 
 def get_token():
     return GlobalVar.ts_token
--- scrapy_gtn/history/stock_quotation.py
+++ scrapy_gtn/history/stock_quotation.py
@@ -1,44 +1,56 @@
 import sys
+import os
+# 临时修改环境变量 为当前目录上两级目录
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(root_dir)
+
 import pandas as pd
 import tushare as ts
 import scrapy_gtn.history.stock_basic as stock_basic
 import scrapy_gtn.conf.config as config
 import logging as log
-import datetime
 import time
+import datetime
+from dateutil.parser import parse
 
 log.basicConfig(level=log.INFO,format=config.get_log_format(),datefmt=config.get_log_datefmt(),
-                handlers={log.FileHandler(filename=config.get_log_dir() + 'stock_d_w_m.log', mode='a', encoding='utf-8')})
+                handlers={log.FileHandler(filename=root_dir + config.get_log_dir() + 'stock_quotation.log', mode='a', encoding='utf-8'),
+                          log.StreamHandler(sys.stderr)})
 
 # 获取所有沪深股票的日、周、月以及分时数据
 
 
-# 沪深股票信息 写入stock_basic.csv
-stock_file_path = stock_basic.get_stock_basic()
-stocks = pd.read_csv(stock_file_path)
+# 沪深股票信息
+stocks = stock_basic.get_stock_basic()
 if(len(stocks) == 0):
     print('获取股票数据失败')
     sys.exit(0)
 
-# 上一次采集数据截止日期,执行前需要确认，便于增量执行
-# 若全量执行，设置为空串 ''
-last_end_date = ''
+# 从参数本次采集开始日期
+if(len(sys.argv) == 1):#未填写开始采集日期
+    log.error('执行出错，请输入数据采集开始日期')
+    sys.exit(1)
+else:
+    try:
+        parse(sys.argv[1])
+    except:
+        log.error('执行出错，数据采集开始日期格式错误，请输入正确的日期yyyyMMdd')
+        sys.exit(1)
+start_date = sys.argv[1]
 
-# 本次采集开始日期
-start_date = ''
-if len(last_end_date) != 0: #增量执行，计算本次执行的开始日期
-    start_date = (datetime.datetime.strptime(last_end_date,'%Y%m%d') + datetime.timedelta(days=1)).strftime('%Y%m%d')
-    print(start_date)
-else: #全量执行(tushare目前最早的数据式19950323)
-    start_date = '19950101'
+# 计算开始日期与当前日期天数差
+days = (datetime.datetime.now().date() - parse(start_date).date()).days
+if (days < 0):
+    log.error('请输入今天之前的日期')
+    sys.exit(1)
 
 # 循环获取各个频率数据
 ts.set_token(config.get_token())
 stock_code_arr = stocks['ts_code']
 
 # 指定字段输出顺序
-cols = ['ts_code','trade_date','open','high','low','close','pre_close','change','pct_chg','vol','amount']
-
+cols1 = ['ts_code','trade_date','open','high','low','close','pre_close','change','pct_chg','vol','amount']
+#接口限制一分钟只能访问120次
 for freq in ['D','W','M']:
     res = pd.DataFrame()
     for ts_code in stock_code_arr:
@@ -46,13 +58,26 @@ for freq in ['D','W','M']:
         df = ts.pro_bar(freq=freq, ts_code=str(ts_code),start_date=start_date)
         res = res.append(df, ignore_index=True)
         log.info('获取---' + str(ts_code) + '---' + freq + '---数据结束')
-        time.sleep(0.5) #接口限制 一分钟只能访问120次
-    res = res[cols]
-    res.to_csv(config.get_data_dir() + 'stock_'+freq+'.csv', index=False,sep=',')
+        time.sleep(0.5)
+    res = res[cols1]
+    res.to_csv(root_dir + config.get_data_dir() + 'stock_'+freq+'.csv', index=False,sep=',')
 
-# 分时数据，接口限制一分钟只能取访问5次
-# for freq in ['1MIN','5MIN','15MIN','30MIN','60MIN']:
-#     res = pd.DataFrame()
-#     for ts_code in stock_code_arr:
-#         log.info('获取---' + str(ts_code) + '---' + freq + '---数据开始')
-#         df = ts.pro_bar(freq=freq, ts_code=str(ts_code),start_date=start_date)
+
+# 指定字段输出顺序
+cols2 = ['ts_code','trade_date','trade_time','open','high','low','close','pre_close','vol','amount']
+# 接口限制一分钟只能取访问5次
+for freq in ['1MIN','5MIN','15MIN','30MIN','60MIN']:
+    res = pd.DataFrame()
+    for ts_code in stock_code_arr:
+        log.info('获取---' + str(ts_code) + '---' + freq + '---数据开始')
+        for i in range(0,days):
+            get_date = (parse(start_date).date() + datetime.timedelta(days=i)).strftime('%Y%m%d')
+            log.info('---正在获取'+get_date+'日期的数据...')
+            df = ts.pro_bar(freq=freq, ts_code=str(ts_code),start_date=get_date)
+            res = res.append(df, ignore_index=True)
+            log.info('---ok')
+            time.sleep(15)
+        log.info('获取---' + str(ts_code) + '---' + freq + '---数据结束')
+        time.sleep(15)
+    res = res[cols2]
+    res.to_csv(root_dir + config.get_data_dir() + 'stock_'+freq+'.csv', index=False,sep=',')
